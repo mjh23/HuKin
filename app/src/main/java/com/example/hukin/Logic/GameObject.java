791b1c85d16ba44dd1086ff96d1f1a7d14bcce85
irksomer <irk2@illinois.edu>
2020-04-22 16:34:24 -0500

    Added basic GameObject class. We can use this as the basis of enemies, the player avatar, and obstacles.

---
+++ app/src/main/java/com/example/hukin/Logic/GameObject.java
@@ -0,0 +1,61 @@
+package com.example.hukin.Logic;
+
+/**
+ * Defines the basic qualities of any object we create in the game.
+ */
+public abstract class GameObject {
+    /** width of image. */
+    protected final int width;
+    /** height of image. */
+    protected final int height;
+    /** x-coordinate of image location. */
+    protected int x;
+    /** y-coordinate of image location. */
+    protected int y;
+
+    /**
+     * Constructs Game Object.
+     * @param setWidth sets width of the object's avatar
+     * @param setHeight sets height of the object's avatar
+     * @param setX sets the x-coordinate of the objects location
+     * @param setY sets the y-coordinate of the objects location
+     */
+    public GameObject(final int setWidth, final int setHeight, final int setX, final int setY) {
+        width = setWidth;
+        height = setHeight;
+        x = setX;
+        y = setY;
+    }
+
+    /**
+     * Getter for avatar width.
+     * @return width of the object avatar
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Getter for avatar height.
+     * @return height of the object's avatar
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Getter for the x-ccordinate of the object's location.
+     * @return x-coordinate of the location
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Getter for the y-coordinate of the object's location.
+     * @return y-coordinate of the location
+     */
+    public int getY() {
+        return y;
+    }
+}
