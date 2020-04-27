527c8355a59f27ac2a70943354b6d497913315b3
irksomer <irk2@illinois.edu>
2020-04-27 10:54:33 -0500

    Added Enemy class and Moveable class. Sorry that this overrides the changes that you just made.

---
+++ app/src/main/java/com/example/hukin/Logic/Enemy.java
@@ -0,0 +1,7 @@
+package com.example.hukin.Logic;
+
+public class Enemy extends Moveable {
+    Enemy (final int x, final int y, final CharacterSprites setSprite, final double setSpeed) {
+        super(x, y, setSprite, setSpeed);
+    }
+}
--- app/src/main/java/com/example/hukin/Logic/GameObject.java
+++ app/src/main/java/com/example/hukin/Logic/GameObject.java
@@ -4,11 +4,7 @@ package com.example.hukin.Logic;
  * Defines the basic qualities of any object we create in the game.
  */
 public abstract class GameObject {
-    /** width of image. */
-    protected final int width;
-    /** height of image. */
-    protected final int height;
-    /** x-coordinate of image location. */
+
     protected int x;
     /** y-coordinate of image location. */
     protected int y;
@@ -16,35 +12,16 @@ public abstract class GameObject {
 
     /**
      * Constructs Game Object.
-     * @param setWidth sets width of the object's avatar
-     * @param setHeight sets height of the object's avatar
      * @param setX sets the x-coordinate of the objects location
      * @param setY sets the y-coordinate of the objects location
      */
-    public GameObject(final int setWidth, final int setHeight, final int setX, final int setY,
+    public GameObject(final int setX, final int setY,
                       final CharacterSprites setSprite) {
-        width = setWidth;
-        height = setHeight;
         x = setX;
         y = setY;
         sprite = setSprite;
     }
 
-    /**
-     * Getter for avatar width.
-     * @return width of the object avatar
-     */
-    public int getWidth() {
-        return width;
-    }
-
-    /**
-     * Getter for avatar height.
-     * @return height of the object's avatar
-     */
-    public int getHeight() {
-        return height;
-    }
 
     /**
      * Getter for the x-ccordinate of the object's location.
@@ -63,4 +40,6 @@ public abstract class GameObject {
     }
     public void setX(int newX) { x = newX; }
     public void setY(int newY) { y = newY; }
+
+    public CharacterSprites getSprite() { return sprite; }
 }
